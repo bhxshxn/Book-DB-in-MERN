db1448abcf3c0b0817c8198a78a1ee2394538fcc
Bhushan Chaudhary <brchadhary9@gmail.com>
2021-08-09 20:29:45 +0530

    initial commit

---
+++ client/src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Main() {
+    return (
+        <div className="flex">
+            <h1 className="head">Book Database</h1>
+        </div>
+    )
+}
+
+export default Main
